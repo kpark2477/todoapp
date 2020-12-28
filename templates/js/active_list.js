d23665469771ae44834881817be8e13ef8a82ac8
kyuwon <kpark2477@gmail.com>
2020-12-28 20:01:25 +0900

    4th active page all fucntions without refresh

--- templates/js/active_list.js
+++ templates/js/active_list.js
@@ -4,10 +4,12 @@
 const todolist_input = document.getElementById('todolist-input')
 document.getElementById('list-form').onsubmit = function (e) {
     e.preventDefault();
+    const newListName = todolist_input.value;
+    todolist_input.value = '';
     fetch('/lists/create', {
         method: 'POST',
         body: JSON.stringify({
-            'name' : todolist_input.value
+            'name' : newListName
         }),
         headers: {
             'Content-type': 'application/json'
@@ -17,8 +19,35 @@ document.getElementById('list-form').onsubmit = function (e) {
         return response.json();
     })
     .then(function (JsonResponse) {
+        console.log('response', JsonResponse);
+
+        const li = document.createElement('li');
+
+        const listCheckbox = document.createElement('input');
+        listCheckbox.className = 'list-check-completed';
+        listCheckbox.type = 'checkbox';
+        listCheckbox.setAttribute('data-id', JsonResponse.id);
+        li.appendChild(listCheckbox);
+        listCheckbox.onchange = (e) => {
+            listCheckBoxChange(e)
+        }
+
+        const listName = document.createElement('a');
+        listName.href = JsonResponse.id
+        listName.innerHTML = ' ' + JsonResponse.name
+        li.appendChild(listName);
+
+        const listDeleteBtn = document.createElement('button');
+        listDeleteBtn.className = 'delete-list';
+        listDeleteBtn.setAttribute('data-id', JsonResponse.id);
+        listDeleteBtn.innerHTML = '&cross;';
+        li.appendChild(listDeleteBtn);
+        listDeleteBtn.onclick = (e) => {
+            deleteList(e)
+        }
+
+        document.getElementById('lists').appendChild(li);
         document.getElementById('error').className = 'hidden';
-        window.location.reload(true);
     })
     .catch(function (err) {
         document.getElementById('error').className = '';
@@ -27,47 +56,63 @@ document.getElementById('list-form').onsubmit = function (e) {
 
 /* delete list */
 const deleteListBtns = document.querySelectorAll(".delete-list");
+const deleteList = function (e) {
+    console.log("Delete event: ", e);
+    const listId = e.target.dataset.id;
+
+    fetch('/lists/' + listId + '/delete', {
+        method: 'DELETE'
+    })
+    .then(function () {
+        if ( listId == {{ active_list.id }} ) {
+            window.location.replace("/")
+        } else {
+            const item = e.target.parentElement;
+            item.remove();
+        }
+    })
+    .catch(function (e) {
+        console.error(e);
+        document.getElementById("error").className = "";
+    })
+}
 for (let i = 0; i < deleteListBtns.length; i++) {
     const deleteBtn = deleteListBtns[i];
-    deleteBtn.onclick = function (e) {
-        console.log("Delete event: ", e);
-        const listId = e.target.dataset.id;
-
-        fetch('/lists/' + listId + '/delete', {
-            method: 'DELETE'
-        })
-        .then(function () {
-            window.location.reload(true);
-        })
-        .catch(function (e) {
-            console.error(e);
-            document.getElementById("error").className = "";
-        });
-        
+    deleteBtn.onclick = (e) => {
+        deleteList(e)
     }
 }
 
 /* list checkbox */
 const listCheckboxes = document.querySelectorAll('.list-check-completed');
-for (let i = 0; i < listCheckboxes.length; i++) {
-    const checkbox = listCheckboxes[i];
-    
-    checkbox.onchange = function (e) {
-        if (e.target.checked) {
-            const listId = e.target.dataset.id;
-            fetch('/lists/' + listId + '/set-completed', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
+const listCheckBoxChange = function (e) {
+    if (e.target.checked) {
+        const listId = e.target.dataset.id;
+        fetch('/lists/' + listId + '/set-completed', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(function (jsonResponse) {
+            document.getElementById('error').className = 'hidden';
+            if (e.target.dataset.id == {{ active_list.id }}) {
+                const todoCheckboxes = document.querySelectorAll('.todo-check-completed');
+                for (let i = 0; i < todoCheckboxes.length; i++) {
+                    const checkbox = todoCheckboxes[i];
+                    checkbox.checked = true;
                 }
-            })
-            .then(function (jsonResponse) {
-                document.getElementById('error').className = 'hidden';
-            })
-            .catch(function () {
-                document.getElementById('error').className= '';             
-            })
-        }       
+            }
+        })
+        .catch(function () {
+            document.getElementById('error').className= '';             
+        })
+    }    
+}
+for (let i = 0; i < listCheckboxes.length; i++) {
+    const checkbox = listCheckboxes[i]; 
+    checkbox.onchange = (e) => {
+        listCheckBoxChange(e)
     }
 }
 
