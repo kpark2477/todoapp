5a0cd4c103192dd0c864d4cffdea615a686c728d
kyuwon <kpark2477@gmail.com>
2020-12-28 13:46:49 +0900

    2nd split templates (main and active_list)

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def delete_todo(todo_id):
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template(
-        'index.html',
+        'main.html',
         lists = TodoList.query.all(), 
         active_list = TodoList.query.get(list_id),
         todos=Todo.query.filter_by(list_id=list_id).order_by('id').all()
